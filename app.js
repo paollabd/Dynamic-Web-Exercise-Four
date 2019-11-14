86a3ac9cb30fbbee7c37b32e14d1accef6a3ad3d
Paolla <pbd244@nyu.edu>
2019-11-14 16:09:11 -0500

    displaying data

--- app.js
+++ app.js
@@ -1,16 +1,14 @@
 const express = require('express');
 const app = express();
-const port = 4000;
+const port = process.env.PORT || 4000;
 
+// routes
 const indexRoute = require('./routes/index.js');
 const aboutRoute = require('./routes/about.js');
 
+// static file serving
+app.use('/static', express.static('public'))
 app.use('/', indexRoute);
 app.use('/about', aboutRoute);
-
-// app.get('/', (reg, res) => res.send('Hello World!'))
-
-// Serve static images
-app.use('/static', express.static('public'))
 
 app.listen(port, () => console.log(`Example app listening on port ${port}!`))
--- routes/index.js
+++ routes/index.js
@@ -1,6 +1,61 @@
 const express = require('express');
 const router = express.Router();
+const firebase = require('firebase');
 
-router.get('/', (req, res) => (res.send('Hello World!')));
+var firebaseConfig = {
+	apiKey: process.env.FIREBASE_API_KEY ,
+	authDomain: "pbd244-exercise-four.firebaseapp.com",
+	databaseURL: "https://pbd244-exercise-four.firebaseio.com",
+	projectId: "pbd244-exercise-four",
+};
 
-module.exports = router; // exporting the router
+const firestoreDatabase = firebase.initializeApp(firebaseConfig);
+const bd = firestoreDatabase.firestore();
+
+let posts = [];
+bd.collection('blog-posts').get()
+	.then(
+		blogPosts => {
+			blogPosts.forEach(post => { // returns the JSON file so we can dsiplay it on the web page
+				posts.push(post.data());
+			})
+			console.log('blogPosts', blogPosts);
+		}
+	)
+	.catch(err => {
+			console.log('error', err) 
+	})
+
+router.get('/', (req, res) => {
+	res.send(posts);
+})
+
+module.exports = router;
+
+// const admin = require("firebase-admin");
+// const express = require('express');
+// const router = express.Router();
+
+// let serviceAccount = require("../pbd244-exercise-four-firebase-adminsdk-wr2na-15a359424d.json");
+
+// admin.initializeApp({
+//   credential: admin.credential.cert(serviceAccount)
+// });
+
+// let db = admin.firestore();
+
+
+// router.get('/', (req, res) => (
+// 	res.send(db.collection('blog-posts').get()
+// 	  .then((snapshot) => {
+// 	    snapshot.forEach((doc) => {
+// 	      console.log(doc.id, '=>', doc.data());
+// 	    });
+// 	  })
+// 	  .catch((err) => {
+// 	    console.log('Error getting documents', err);
+// 	  })
+
+// )));
+
+// module.exports = router; // exporting the router
